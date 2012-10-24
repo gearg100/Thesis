7356590ec5bfb07c793ed0305d88b5bb03a3f57b
Georgios Psaropoulos <gearg100@windowslive.com>
2012-10-24 18:17:37 +0300

    Revert "Added Scala Concordance"
    This reverts commit 929098e9289b2007293cc8ab338f88ff6858776a.

--- Concordance/F#/src/Concordance.fs
+++ Concordance/F#/src/Concordance.fs
@@ -8,10 +8,13 @@ open Microsoft.FSharp.Collections
 
 type Data() =
     let mutable n = 0
-    let lst = ResizeArray<int>()
-    member x.Count = n
-    member x.Positions = Seq.toList lst
-    member x.IncCount value = n <- n + value
+    let lst = System.Collections.Generic.List<int>()
+    member x.Count 
+        with get() = n
+        and internal set(value) = 
+            n <- n+1
+    member x.Positions 
+        with get() = Seq.toList lst
     member internal x.List = lst
     override x.ToString() = 
          lst.Aggregate(
@@ -19,15 +22,15 @@ type Data() =
             fun builder current -> builder.Append (string current + "; ")
          ).ToString() + "])"
     static member (+=) (data : Data,n :int) = 
-        data.IncCount 1
+        data.Count <- data.Count + 1
         data.List.Add n
     static member (++=) (data : Data, data2 : Data) =
-        data.IncCount data2.Count
+        data.Count <- data.Count + data2.Count
         data.List.AddRange data2.List
 
 
-type ConcordanceMap<'a when 'a : equality>() =
-    inherit System.Collections.Generic.Dictionary<'a, Data>()
+type ConcordanceMap() =
+    inherit System.Collections.Generic.Dictionary<string list, Data>()
     member x.Item2 
         with get(index) = 
             let def = Data()
@@ -43,74 +46,79 @@ type ConcordanceMap<'a when 'a : equality>() =
 module Agents = 
     open Concordance.SeqTransformation
     open Concordance
-    type Message1<'a when 'a : equality> =
+    type Message1 =
         |Start
-        |Finished of int * ConcordanceMap<'a>
+        |Finished of int * ConcordanceMap
 
-    type Message2<'a> =
+    type Message2 =
         |Stop
-        |Work of ('a * int) seq
+        |Work of (string list * int) seq
 
     let worker (master:MailboxProcessor<_>) i =
         MailboxProcessor.Start(fun inbox ->
             let map = ConcordanceMap()
             let rec loop ()= async {
-                try
-                    let! msg = inbox.Receive()
-                    match msg with
-                    |Work(bulk) ->
-                        for (seq, n) in bulk do
-                            map.Item2(seq) += n
-                        //printfn "%A done" (Seq.toList bulk)
-                        return! loop()
-                    |Stop ->
-                        //printfn "Stopped %d" i
-                        master.Post <| Finished(i,map)
-                with e ->
-                    printfn "%s" e.Message
+                let! msg = inbox.Receive()
+                match msg with
+                |Work(bulk) ->
+                    for (seq, n) in bulk do
+                        map.Item2(seq) += n
+                    //printfn "%A done" (Seq.toList bulk)
+                    return! loop()
+                |Stop ->
+                    //printfn "received Stop"
+                    //printfn "Stopped %d" i
+                    master.Post <| Finished(i,map)
             }
             loop()
         )        
 
-    let inline indexOf N (str,_) = (hash str) % N |> abs //(hash lst) &&& (N-1)  //optimal??
+    let inline indexOf (lst,_)  N = (abs (hash lst)) % N//(hash lst) &&& (N-1)     
 
     let master S G M N input (worker: MailboxProcessor<_> -> int -> MailboxProcessor<_>)onComplete =
         MailboxProcessor.Start(fun inbox ->
             let workers = Array.init N <| worker inbox
             let res = ResizeArray(N)
             let rec finishing j = async {
-                try
-                    let! msg = inbox.Receive()
-                    match msg with
-                    |Finished (i, map) ->
-                        res.Add(map)
-                        //printfn "%d finished with %d keys" i map.Count
-                        if j > 0 then 
-                            return! finishing (j - 1)
-                        else
-                            onComplete(res)
-                    | _ -> printfn "%A : unknown" msg
-                with e ->
-                    printfn "%s" e.Message
-                    
+                let! msg = inbox.Receive()
+                match msg with
+                |Finished (i, map) ->
+                    res.Add(map)
+                    //printfn "%d finished with %d keys" i map.Count
+                    //Seq.iter (fun pair -> printfn "%A" pair) map
+                    if j > 0 then 
+                        return! finishing (j - 1)
+                    else
+                        onComplete(res)
+                | _ -> printfn "%A : unknown" msg
             }
             let start() = async {
                 let! msg = inbox.Receive()
                 try 
                     match msg with
                     |Start ->
-                        let inline func (sq:#seq<_>) = 
-                            try
-                                for group in sq.GroupBy(Func<_,_>(indexOf N)) do
-                                    workers.[group.Key].Post <| Work group
-                            with 
-                            | e-> 
-                                printfn "Error: %s" e.Message
                         sequences input S
                         |> Seq.chunked G
-                        |> PSeq.``chunked iter(PLINQ)`` M func
+                        |> Seq.chunked M
+                        |> Seq.iter (fun chunk -> 
+                            let inline func (seq:#seq<_>) = 
+                                let bulks = seq.GroupBy (fun seq -> indexOf seq N)
+                                try
+                                    for group in bulks do
+                                        workers.[group.Key].Post <| Work group
+                                with 
+                                | e-> 
+                                    printfn "Error: %s" e.Message
+                            // Using TPL
+                            let factory = Threading.Tasks.Task.Factory
+                            let tasks = chunk.Select(fun it -> factory.StartNew(fun () -> func(it))).ToArray()
+                            System.Threading.Tasks.Task.WaitAll(tasks)
+                            //Using PLinq
+                            //chunk.AsParallel().WithDegreeOfParallelism(M).ForAll(fun el -> func el)
+                        )
                         workers
                         |> Seq.iter (fun worker -> worker.Post Stop)
+                        //printfn "All Stop sent"
                         return! finishing (N - 1)
                     | _ -> printfn "%A : unknown" msg
                 with e -> printfn "Error"
@@ -126,7 +134,7 @@ module Execution =
         let words = time T <| fun () -> readFile file |> Seq.toList
         print("\nBuilding HashMap... ")
         let result = time T  <| fun () ->
-            let r: ResizeArray<ConcordanceMap<_>> ref= ref null
+            let r: ResizeArray<ConcordanceMap> ref= ref null
             let latch = new System.Threading.ManualResetEvent(false)
             let master = master S G M N words worker <| fun res ->
                 r := res
@@ -134,7 +142,7 @@ module Execution =
             master.Post Start
             latch.WaitOne()|> ignore
             r
-        Console.WriteLine ("\nTotal items in HashMap: " + result.Value.Sum(fun (map:ConcordanceMap<_>) -> map.Values.Sum(fun (v:Data) -> v.Count)).ToString())
+        Console.WriteLine ("\nTotal items in HashMap: " + result.Value.Sum(fun (map:ConcordanceMap) -> map.Values.Sum(fun (v:Data) -> v.Count)).ToString())
 
 
 
--- Concordance/Scala/src/test/scala/concordance/test/ConcordTest.scala
+++
@@ -1,9 +0,0 @@
-package concordance.test
-
-object ConcordTest extends App{
-  import common.test
-  import concordance._
-  
-  println(toConcordanceList(test, 3).toList)
-
-}
