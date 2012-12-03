f39e7c41c89c2db6d156ff18925cf015738cda05
Georgios Psaropoulos <gearg100@windowslive.com>
2012-12-03 22:14:30 +0200

    Updated script to work in Unixoids

--- Orbit/F#/run.fsx
+++ Orbit/F#/run.fsx
@@ -15,17 +15,28 @@ let run (psi:ProcessStartInfo) (args:string) =
     proc.StandardOutput.ReadToEnd()
 
 let psi = 
-    ProcessStartInfo( 
-        FileName = @"./target/Orbit.exe",
-        UseShellExecute = false,
-        RedirectStandardInput = true,
-        RedirectStandardOutput = true
-    )
+    if Environment.OSVersion.Platform = PlatformID.Unix then
+        ProcessStartInfo( 
+            FileName = @"mono",
+            Arguments = @"--gc=sgen --runtime=v4.0 ./target/Orbit.exe",
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true
+        )
+    else
+        ProcessStartInfo( 
+            FileName = @"./target/Orbit.exe",
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true
+        )
 
-Console.WriteLine("nOfTimes each test will run: ")
+Console.Write("nOfTimes each test will run: ")
 let times = int <| Console.ReadLine()
 do 
     use writer = new StreamWriter(@"times",true)
+    writer.AutoFlush <- true
     for M in [1;2;4;8;16;32;64] do
       for N in [for i in [1;2;4;8;16] do if i <= M then yield i] do
         for G in [1000; 5000; 10000; 20000] do
@@ -45,8 +56,10 @@ do
                 let (TimeElapsed timeElapsed) = lines.[1]
                 totalTimeElapsed <- totalTimeElapsed + int timeElapsed
                 res <- int64 result
-            sprintf "%d, %d, %d, %d, %d" M N G res (totalTimeElapsed/times)
-            |> writer.WriteLine
+            let str = sprintf "%d, %d, %d, %d, %d" M N G res (totalTimeElapsed/times)
+            Console.WriteLine(str)
+            writer.WriteLine(str)
+            
 
 
 
